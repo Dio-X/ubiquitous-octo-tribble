6ad571877b19aaf246ffb780c50a44c4e11d771a
Aviral <128823099+Dio-X@users.noreply.github.com>
2026-07-12 15:19:55 +0000

    util code 1.0

--- src/index.js
+++ src/index.js
@@ -2,32 +2,25 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import app from "./app.js"
+
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then(()=>{
+    app.on("errror",error =>{
+        console.log("ERRR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server running on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!",err)
+})
 
 
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror",(error) => {
-            console.log("ERRR: ",error);
-            throw error
-        })
-        app.listen(process.env.PORT,() => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch(err){
-        console.error("ERROR: ",err)
-        throw err
-    }
-})()
-
-*/
