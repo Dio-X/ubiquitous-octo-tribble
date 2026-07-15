e02f0e8e28d9619113afac3636e631bf8aab8622
Aviral <128823099+Dio-X@users.noreply.github.com>
2026-07-15 12:42:40 +0000

    setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv"
+import userRouter from "./routes/user.routes.js";
+
 
 dotenv.config({
     path: "./env"
@@ -19,5 +21,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+app.use("/api/v1/user",userRouter)
 
 export default app;
