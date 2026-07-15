dcb828f11bd1865a293df245f5c8be4a1bd260a5
Aviral <128823099+Dio-X@users.noreply.github.com>
2026-07-15 13:16:26 +0000

    add register controller and working

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,41 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({message:"user registered"})
+    const {fullName,email,username,password} = req.body;
+    if([fullName,email,username,password].some(field => {
+        field?.trim() === ""
+    })){
+        throw new ApiError(400,"All fields are required")
+    }
+    const existingUser = User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(existingUser){
+        throw new ApiError(409,"User with given username or email already exists")
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
 })
 
 export {
